3de57714e3369927d63716b096f64942f5a0b5fd
Brian Lindner <cslindner@gmail.com>
2016-02-12 22:45:28 -0500

    made the console font solution work better

--- config.sh
+++ config.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 #i3
-echo "linking i3-gaps configuration...\n"
+echo "linking i3-gaps configuration..."
 mkdir ~/.config
 ln -sf i3 ~/.config
 ln -sf i3status ~/.config
 
 # nano, screen and bash
-echo "linking nano, screen, and bash configuration...\n"
+echo "linking nano, screen, and bash configuration..."
 ln -f .nanorc ~
 ln -f .screenrc ~
 ln -f .bash_profile ~
 ln -f .bashrc ~
 
 # terminal font
-echo "setting console font... (this may only work on debian)"
-sudo setfont /usr/share/consolefonts/Uni3-Terminus14.psf.gz
+echo "linking console-setup..."
+sudo rm /etc/default/console-setup
+sudo cp console-setup /etc/default/console-setup
+
+echo "done!"
