2d993a738d7dd30bbf145c0a8ca34b7a3af97179
Brian Lindner <cslindner@gmail.com>
2016-02-12 22:01:14 -0500

    fleshed out config, and added tamzen as console font

--- config.sh
+++ config.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+#i3
+echo "linking i3-gaps configuration...\n"
 mkdir ~/.config
-ln -fs i3 ~/.config
-ln -fs i3status ~/.config
-ln -fs .nanorc ~
-ln -fs .screenrc ~
-ln -fs .bash_profile ~
-ln -fs .bashrc ~
+ln -sf i3 ~/.config
+ln -sf i3status ~/.config
+
+# nano, screen and bash
+echo "linking nano, screen, and bash configuration...\n"
+ln -f .nanorc ~
+ln -f .screenrc ~
+ln -f .bash_profile ~
+ln -f .bashrc ~
+
+# tamsyn 1.11 as terminal font - s/o to Scott Fial!
+echo "setting console font...\n"
+wget http://www.fial.com/~scott/tamsyn-font/download/tamsyn-font-1.11.tar.gz
+tar -xzvf tamsyn-font-1.11.tar.gz tamsyn-font-1.11/Tamsyn7x14r.psf.gz
+cp tamsyn-font-1.11/Tamsyn7x14r.psf.gz ~/.config/
+sudo setfont ~/.config/Tamsyn7x14r.psf.gz
+rm -rf tamsyn*
