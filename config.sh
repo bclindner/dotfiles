555447d8314dc14e1193b70932851dcba7a87b4d
bclindner <brian@bclindner.com>
2017-04-08 15:09:58 -0400

    fixed config.sh

--- config.sh
+++ config.sh
@@ -14,19 +14,4 @@ echo "copying console-setup..."
 sudo rm /etc/default/console-setup
 sudo cp console-setup /etc/default/console-setup
 
-#i3
-echo "setup i3? [y/n]"
-read -s -n 1 -r char
-if [ $char = "y" ]; then
-  echo "linking i3-gaps configuration & lemonbar..."
-  mkdir ~/.config
-  ln -sf $(pwd)/i3 ~/.config/i3
-fi
-echo "setup xfce4-terminal? [y/n]"
-read -s -n 1 -r char
-if [ $char = "y" ]; then
-  echo "linking xfce4-terminal configuration..."
-  ln -sf $(pwd)/xfce4/terminal ~/.config/xfce4
-fi
-
 echo "done!"
