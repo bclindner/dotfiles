30cb174068da1885cd291925302ed09133fd4a49
Brian C. Lindner <brian@bclindner.com>
2017-04-08 17:40:50 -0400

    fixed the revert & added xfce bar

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # nano, screen and bash
-echo "linking screen, and bash configuration..."
+echo "linking nano, screen, and bash configuration..."
+ln -f $(pwd)/.nanorc ~
 ln -f $(pwd)/.screenrc ~
 ln -f $(pwd)/.bash_profile ~
 ln -f $(pwd)/.bashrc ~
@@ -12,5 +13,20 @@ ln -f $(pwd)/.xresources ~
 echo "copying console-setup..."
 sudo rm /etc/default/console-setup
 sudo cp console-setup /etc/default/console-setup
+
+#i3
+echo "setup i3? [y/n]"
+read -s -n 1 -r char
+if [ $char = "y" ]; then
+  echo "linking i3-gaps configuration & lemonbar..."
+  mkdir ~/.config
+  ln -sf $(pwd)/i3 ~/.config/i3
+fi
+echo "setup xfce4-terminal? [y/n]"
+read -s -n 1 -r char
+if [ $char = "y" ]; then
+  echo "linking xfce4-terminal configuration..."
+  ln -sf $(pwd)/xfce4/terminal ~/.config/xfce4
+fi
 
 echo "done!"
