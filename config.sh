6e7fbb762eccb1e7fac3d3a993c19508b9b929e4
Brian Lindner <brian@bclindner.com>
2017-04-12 11:28:11 -0400

    added zsh

--- config.sh
+++ config.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
+# dotfile configuration program
 
-# nano, screen and bash
-echo "linking nano, bash, and xresources configuration..."
-ln -f $(pwd)/.nanorc ~
-ln -f $(pwd)/.bashrc ~
-ln -f $(pwd)/.xresources ~
-# screen
-if type screen 2>/dev/null; then
-  echo "screen detected, adding configuration..."
-  ln -f $(pwd)/.screenrc ~
-  ln -f $(pwd)/.bash_profile ~
+# zsh/bash configuration
+if [ $SHELL = "/bin/zsh" ]; then
+  echo "install shell configuration files? (zsh)"
+  read -s -n 1 -r char
+  ln -f $(pwd)/.bashrc ~
+  if type screen 2>/dev/null; then
+    echo "screen detected. add configuration?"
+    read -s -n 1 -r char
+    if [ $char = "y" ]; then
+      ln -f $(pwd)/.screenrc ~
+      ln -f $(pwd)/.zprofile ~
+    fi
+  fi
+fi
+if [ $SHELL = "/bin/bash" ]; then
+  echo "install shell configuration files? (bash)"
+  read -s -n 1 -r char
+  ln -f $(pwd)/.bashrc ~
+  if type screen 2>/dev/null; then
+    echo "screen detected. add configuration?"
+    read -s -n 1 -r char
+    if [ $char = "y" ]; then
+      ln -f $(pwd)/.screenrc ~
+      ln -f $(pwd)/.bash_profile ~
+    fi
+  fi
 fi
 
+# xresources 
+ln -f $(pwd)/.xresources ~
+
 # terminal font
-echo "setup console-setup? (requires root) [y/n]"
+echo "setup console-setup? (requires root) "
 read -s -n 1 -r char
 if [ $char = "y" ]; then
   echo "copying console-setup..."
@@ -22,14 +42,14 @@ if [ $char = "y" ]; then
 fi
 
 #i3
-echo "setup i3? [y/n]"
+echo "setup i3? "
 read -s -n 1 -r char
 if [ $char = "y" ]; then
   echo "linking i3-gaps configuration & lemonbar..."
   mkdir ~/.config
   ln -sf $(pwd)/i3 ~/.config/i3
 fi
-echo "setup xfce4? [y/n]"
+echo "setup xfce4? "
 read -s -n 1 -r char
 if [ $char = "y" ]; then
   echo "linking xfce4 configuration..."
