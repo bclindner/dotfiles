c4512b8734f4cb4ede3c386ffeae3f792efe1928
Brian C. Lindner <brian@bclindner.com>
2018-10-04 13:57:19 -0400

    polybar now has multi-monitor support

--- desktop/polybar/i3-launch.sh
+++ desktop/polybar/i3-launch.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
+# kill all polybar instances
 killall -q polybar
 
+# wait for polybar instances to die
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar bclindner &
+
+# set bar to launch
+BARTYPE=bclindner
+
+# check if xrandr is on this machine
+if type "xrandr"; then
+  # if so, get all the connected screens
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    # launch a bar for each screen
+    MONITOR=$m polybar --reload $BARTYPE &
+  done
+else
+  # if there's no xrandr, just launch the bar
+  polybar --reload $BARTYPE &
+fi
